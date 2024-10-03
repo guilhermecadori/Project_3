f67892fcd895036e2c21e2d0e4ab61453db04db4
guilhermecadori <gccadori@gmail.com>
2024-10-03 00:46:56 -0300

    first commit

---
+++ practice_20241002.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello, World!" << std::endl; // Flushes the output buffer
+    std::cout << "Hello again!" << std::endl;
+
+    std::cout << "Hello, World!" << '\n'; // '\n' is better, performance wise
+    std::cout << "Hello again!" << '\n'; 
+
+    return 0;
+}
+
