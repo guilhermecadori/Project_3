19aa3a6f837ecf8a182f69f6f97e21cda1011bc9
guilhermecadori <gccadori@gmail.com>
2024-10-03 01:21:35 -0300

    Updated cpp file and cloned repo to local machine

--- practice_20241002.cpp
+++ practice_20241002.cpp
@@ -2,13 +2,13 @@
 
 int main() {
 
+
     std::cout << "Hello, World!" << std::endl; // Flushes the output buffer
     std::cout << "Hello again!" << std::endl;
+    std::cout << "Hello, World! But using a different method..." << '\n'; // '\n' is better, performance wise
+    std::cout << "Hello again! But using a different method..." << '\n'; 
 
-    std::cout << "Hello, World!" << '\n'; // '\n' is better, performance wise
-    std::cout << "Hello again!" << '\n'; 
 
     return 0;
 
 }
-
