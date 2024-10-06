78c84f561bdd4bcc48c2cd8f57d87e195e620003
guilhermecadori <gccadori@gmail.com>
2024-10-05 22:55:38 -0300

    New commits

--- practice_20251005.cpp
+++ practice_20251005.cpp
@@ -1,6 +1,5 @@
-// Bro Code
-
-// const: The const keyword is used to indicate to the compile that the value of a variables cannot be changed
+// const: The const keyword specifies that a variable's values is constant
+//        It indicates to the compiler to prevent anything from modifying it
 //        This means that the variable is read-only after being created
 
 # include <iostream>
@@ -8,9 +7,10 @@
 int main() {
 
     // Calculating the circumference of a circle
-    double pi = 3.14159;
+    const double PI = 3.14159; // Add prefix 'const' and, by convention, use upper case only to name constant variable
+    // PI = 510 // Uncommenting this line will result in an error
     double radius = 10;
-    double circumference = 2 * pi * radius;
+    double circumference = 2 * PI * radius;
 
     std::cout << "Circumference : " << circumference << " cm." << std::endl;
 
