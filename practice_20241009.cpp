80986e7484729109385d015442e9ade8eab22a65
guilhermecadori <gccadori@gmail.com>
2024-10-09 23:57:32 -0300

    New

--- practice_20241009.cpp
+++ practice_20241009.cpp
@@ -22,9 +22,16 @@ std::string repeat_str(size_t repeat, const std::string& str) {
 
 }
 
-
 // Ex 2
 /*
+    You're writing code to control your town's traffic lights. 
+    You need a function to handle each change from green, to yellow, 
+    to red, and then to green again.
 
+    Complete the function that takes a string as an argument representing 
+    the current state of the light and returns a string representing the s
+    tate the light should change to.
 
+    For example, when the input is green, output should be yellow.
 */
+
