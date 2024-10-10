a6bd5411cdae82b99c87ca7f7dbae6545fee26bf
guilhermecadori <gccadori@gmail.com>
2024-10-09 23:52:09 -0300

    The see me rolling, they hatin'

---
+++ practice_20241009.cpp
@@ -0,0 +1,28 @@
+// Ex 1
+/*
+    Write a function that accepts an integer n and a string s as parameters, 
+    and returns a string of s repeated exactly n times.
+    
+    Examples (input -> output)
+        6, "I"     -> "IIIIII"
+        5, "Hello" -> "HelloHelloHelloHelloHello"
+*/
+
+#include <string>
+
+std::string repeat_str(size_t repeat, const std::string& str) {
+    
+    std::string str_acumm = ""; 
+
+    for (size_t i=0; i < repeat; i++) {
+        str_acumm += str;
+    };
+
+    return str_acumm; 
+}
+
+int main() {
+
+
+    return 0;
+}
