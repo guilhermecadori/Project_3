11a2c82b8428e7915d2d42701eafe45a170565b9
guilhermecadori <gccadori@gmail.com>
2024-10-11 18:37:18 -0300

    Final practice problem

---
+++ practice_20241011_3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+// Create a program to calculat the hypotenuse of a right angled traingle
+int main() {
+
+    // C = SQRT(A^2 + B^2)
+
+    double A;
+    double B;
+    double C;
+
+    std::cout << "Please provide A: " << std::endl;
+    std::cin >> A;
+
+    std::cout << "Please provide B: " << std::endl;
+    std::cin >> B;
+
+    C = sqrt(pow(A, 2) + pow(B, 2));
+
+    std::cout << "The hypotenuse is: " << C << std::endl;  
+
+    return 0;
+}
