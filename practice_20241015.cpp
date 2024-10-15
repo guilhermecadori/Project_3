7cabfca816fc9aaa73990a3238eabfa8cd8351f0
guilhermecadori <gccadori@gmail.com>
2024-10-15 20:35:58 -0300

    Another day

---
+++ practice_20241015.cpp
@@ -0,0 +1,35 @@
+// Ex 1
+
+/*
+    Complete the square sum function so that it squares each number passed 
+    into it and then sums the results together.
+
+    For example:
+        - for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9
+*/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int square_sum(const std::vector<int>& numbers) {
+
+    int sum_sq = 0;
+
+    for (int val : numbers) {
+        val = val * val;
+        sum_sq += val;
+    }
+
+    return sum_sq;
+}
+
+// Testing 
+int main() {
+
+    std::vector<int> vec = {1, 2, 3};
+
+    square_sum(vec);
+
+    return 0;
+}
