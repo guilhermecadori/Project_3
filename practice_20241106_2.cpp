2030f75ec3ec4b8405ae9c2ed814b5d29f2181be
guilhermecadori <gccadori@gmail.com>
2024-11-06 13:37:01 -0300

    Passed all tests ran locally

--- practice_20241106_2.cpp
+++ practice_20241106_2.cpp
@@ -11,22 +11,58 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 // Function
-std::string sortStrings(std::vector<std::string> stringList) {
-
+std::string sortStrings(std::vector<std::string> stringArray) {
     // Support variables
+    std::vector<std::string> arrayASCII;
+    int letterASCII;
+    std::string wordASCII;
+    std::string firstWordASCII;
+    std::string outputWord;
+    std::string asciiChar;
 
-    
+    // Retrieve ASCII values
+    for (const std::string& word : stringArray) {
+        wordASCII.clear();  // Clear the string for each word
+        for (char c : word) {
+            letterASCII = c;
+            wordASCII.push_back(static_cast<char>(letterASCII));
+        }
 
-    // Sort based on ASCII and letter case
+        // Add new word to array
+        arrayASCII.push_back(wordASCII);
+    }
 
+    // Sort based on ASCII (ASCII is case-sensitive by itself)
+    std::sort(arrayASCII.begin(), arrayASCII.end(), std::greater<>());
 
+    // Adds requested * char between letters
+    firstWordASCII = arrayASCII[0];  // Now firstWordASCII is a string
+    for (char c : firstWordASCII) {
+        asciiChar = c + std::string("***");  // Convert char and append ***
+        outputWord += asciiChar;
+    }
+
+    // Remove last "***"
+    const size_t suffixLength = 3;  // Length of the "***" suffix
+    outputWord.resize(outputWord.size() - suffixLength);
 
     // Return output
-
+    return outputWord;
 }
 
 
 // Test
+int main() {
+
+    std::vector<std::string> testInput = {"Xilofone", "Zarolho", "Abacate", "abacate"};
+
+    std::cout << sortStrings(testInput);
+
+    return 0;
+}
+
+
 
