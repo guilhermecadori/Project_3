054d8e893d84dde1c318e72305e1ebd4e09d1357
guilhermecadori <gccadori@gmail.com>
2024-11-06 12:24:36 -0300

    Deleted .exe s

--- practice_20241106_2.cpp
+++ practice_20241106_2.cpp
@@ -1,1 +1,32 @@
 // Ex 2
+/*
+    You will be given a list of strings. 
+    You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) 
+    and then return the first value.
+
+    The returned value must be a string, and have "***" between each of its letters.
+
+    You should not remove or add elements from/to the array.
+*/
+
+#include <iostream>
+#include <vector>
+
+// Function
+std::string sortStrings(std::vector<std::string> stringList) {
+
+    // Support variables
+
+    
+
+    // Sort based on ASCII and letter case
+
+
+
+    // Return output
+
+}
+
+
+// Test
+
