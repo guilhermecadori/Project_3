99c1fdab48ae69f8ccfb3b40563217ea5c9c3b38
guilhermecadori <gccadori@gmail.com>
2024-11-11 12:59:44 -0300

    This version passed all online tests

---
+++ practice_20241111_3.cpp
@@ -0,0 +1,56 @@
+// Ex 3
+/*
+Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+    "4",  "5" --> "9"
+    "34", "5" --> "39"
+    "", "" --> "0"
+    "2", "" --> "2"
+    "-5", "3" --> "-2"
+
+Notes:
+    If either input is an empty string, consider it as zero.
+    Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+*/
+
+#include <iostream>
+#include <string>
+
+// Function
+std::string stringSumNumber(std::string number1, std::string number2) {
+
+    int intNumber1 = 0;
+    int intNumber2 = 0;
+    int sum;
+    std::string stringSum = "0";
+
+    if (number1.empty()) {
+        number1 = "0";
+    }
+
+    if (number2.empty()) {
+        number2 = "0";
+    }
+
+    intNumber1 = std::stoi(number1);
+    intNumber2 = std::stoi(number2);
+    sum = intNumber1 + intNumber2;
+    stringSum = std::to_string(sum);
+        
+    return stringSum;
+}
+
+
+// Test
+int main() {
+
+    std::string number1Test = "2";
+    std::string number2Test = "6";
+    
+    std::cout << stringSumNumber(number1Test, number2Test);
+
+    return 0;
+}
+
+
