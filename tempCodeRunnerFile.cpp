dceab8c061f560889722983ecb33446eff818e92
guilhermecadori <gccadori@gmail.com>
2024-11-12 16:17:27 -0300

    Adding missing files

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,1 @@
-        int intNumber1 = std::stoi(number1);
-        int intNumber2 = std::stoi(number2);
-        int sum = intNumber1 + intNumber2;
-        std::string stringSum = std::to_string(sum);
+char
